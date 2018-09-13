e6b208b10b23df27dc24a9b6b76d6c48fd2cee13
ThorinSchmidt <tschmidt@stcharlessd.org>
2018-09-13 15:36:13 -0500

    modified user interface. added checkboxes for a-j, and output text box

--- BFTreasure.py
+++ BFTreasure.py
@@ -4,6 +4,8 @@
 # 9/13/2018
 
 from tkinter import *
+from tkinter import ttk
+from tkinter import font
 
 class Application(Frame):
     
@@ -17,7 +19,10 @@ class Application(Frame):
     def create_widgets(self):
         """ create widgets in frame """
         self.lblLair = Label(self, text = "Lair Types")
-        self.lblLair.grid(row = 0, column = 0, columnspan = 5)
+        self.lblLair.grid(row = 0, column = 0, columnspan = 3, sticky="w")
+        
+        ttk.Separator(self, orient=HORIZONTAL).grid(row=1, column=0,
+                                                    columnspan=5, sticky="ew")
         
         self.chkBtnA = BooleanVar()
         Checkbutton(self, text = "A",
@@ -31,24 +36,61 @@ class Application(Frame):
     
         self.chkBtnC = BooleanVar()
         Checkbutton(self, text = "C",
-                    variable = self.chkBtnA
+                    variable = self.chkBtnC
                     ).grid(row = 2, column = 2)
     
         self.chkBtnD = BooleanVar()
         Checkbutton(self, text = "D",
-                    variable = self.chkBtnA
+                    variable = self.chkBtnD
                     ).grid(row = 2, column = 3)
     
         self.chkBtnE = BooleanVar()
         Checkbutton(self, text = "E",
-                    variable = self.chkBtnA
+                    variable = self.chkBtnE
                     ).grid(row = 2, column = 4)
     
-
+        
+        self.chkBtnF = BooleanVar()
+        Checkbutton(self, text = "F",
+                    variable = self.chkBtnF
+                    ).grid(row = 3, column = 0)
+    
+        self.chkBtnG = BooleanVar()
+        Checkbutton(self, text = "G",
+                    variable = self.chkBtnG
+                    ).grid(row = 3, column = 1)
+    
+        self.chkBtnH = BooleanVar()
+        Checkbutton(self, text = "H",
+                    variable = self.chkBtnH
+                    ).grid(row = 3, column = 2)
+    
+        self.chkBtnI = BooleanVar()
+        Checkbutton(self, text = "I",
+                    variable = self.chkBtnI
+                    ).grid(row = 3, column = 3)
+    
+        self.chkBtnJ = BooleanVar()
+        Checkbutton(self, text = "J",
+                    variable = self.chkBtnJ
+                    ).grid(row = 3, column = 4)
+    
+        
+        ttk.Separator(self, orient=HORIZONTAL).grid(row=12, column=0,
+                                                    columnspan=5, sticky="ew")
+        
+        Label(self, text="Treasure Report").grid(row=13, column=0, columnspan=5,
+                                                 sticky="w")
+        
+        self.textBoxReport = Text(self, width = 50, height = 20, wrap = WORD)
+        self.textBoxReport.grid(row=14, column =0, columnspan=5)
 
 #main section
 root = Tk()
 root.title("BF Treasure Generator")
-root.geometry('500x500')
+#root.geometry('500x500')
+default_font = font.nametofont("TkDefaultFont")
+default_font.configure(size=20)
+
 app = Application(root)
 root.mainloop()
