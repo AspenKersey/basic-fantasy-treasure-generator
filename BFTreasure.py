0d839407ac063bb2353281bf7f07c3d40f70013a
Thorin Schmidt <tschmidt@stcharlessd.org>
2018-09-24 16:45:40 -0500

    changed rows
    changed rows

--- BFTreasure.py
+++ BFTreasure.py
@@ -6,6 +6,9 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import font
+from tkinter import simpledialog
+from random import randint
+import TreasureType
 
 class Application(Frame):
     
@@ -20,6 +23,7 @@ class Application(Frame):
         self.ep=0
         self.gp=0
         self.pp=0
+        self.multiplier=1
         
         self.create_widgets()
         
@@ -31,10 +35,10 @@ class Application(Frame):
         
         self.btnLairClear = Button(self, text="Clear",
                                    command = self.clear_lair_types)
-        self.btnLairClear.grid(row=0, column=3, columnspan=2)
+        self.btnLairClear.grid(row=0, column=6, columnspan=2)
         
         ttk.Separator(self, orient=HORIZONTAL).grid(row=1, column=0,
-                                                    columnspan=5, sticky="ew")
+                                                    columnspan=8, sticky="ew")
         
         self.chkBtnA = BooleanVar()
         Checkbutton(self, text = "A",
@@ -65,62 +69,62 @@ class Application(Frame):
         self.chkBtnF = BooleanVar()
         Checkbutton(self, text = "F",
                     variable = self.chkBtnF
-                    ).grid(row = 3, column = 0)
+                    ).grid(row = 2, column = 5)
     
         self.chkBtnG = BooleanVar()
         Checkbutton(self, text = "G",
                     variable = self.chkBtnG
-                    ).grid(row = 3, column = 1)
+                    ).grid(row = 2, column = 6)
     
         self.chkBtnH = BooleanVar()
         Checkbutton(self, text = "H",
                     variable = self.chkBtnH
-                    ).grid(row = 3, column = 2)
+                    ).grid(row = 2, column = 7)
     
         self.chkBtnI = BooleanVar()
         Checkbutton(self, text = "I",
                     variable = self.chkBtnI
-                    ).grid(row = 3, column = 3)
+                    ).grid(row = 3, column = 0)
     
         self.chkBtnJ = BooleanVar()
         Checkbutton(self, text = "J",
                     variable = self.chkBtnJ
-                    ).grid(row = 3, column = 4)
+                    ).grid(row = 3, column = 1)
         
         self.chkBtnK = BooleanVar()
         Checkbutton(self, text = "K",
-                    variable = self.chkBtnA
-                    ).grid(row = 4, column = 0)
+                    variable = self.chkBtnK
+                    ).grid(row = 3, column = 2)
     
         self.chkBtnL = BooleanVar()
         Checkbutton(self, text = "L",
-                    variable = self.chkBtnB
-                    ).grid(row = 4, column = 1)
+                    variable = self.chkBtnL
+                    ).grid(row = 3, column = 3)
     
         self.chkBtnM = BooleanVar()
         Checkbutton(self, text = "M",
-                    variable = self.chkBtnC
-                    ).grid(row = 4, column = 2)
+                    variable = self.chkBtnM
+                    ).grid(row = 3, column = 4)
     
         self.chkBtnN = BooleanVar()
         Checkbutton(self, text = "N",
-                    variable = self.chkBtnD
-                    ).grid(row = 4, column = 3)
+                    variable = self.chkBtnN
+                    ).grid(row = 3, column = 5)
     
         self.chkBtnO = BooleanVar()
         Checkbutton(self, text = "O",
-                    variable = self.chkBtnE
-                    ).grid(row = 4, column = 4)
+                    variable = self.chkBtnO
+                    ).grid(row = 3, column = 6)
 
         self.lblInd = Label(self, text = "Individual Types")
         self.lblInd.grid(row = 5, column = 0, columnspan = 3, sticky="w")
         
         self.btnIndClear = Button(self, text="Clear",
                                    command = self.clear_ind_types)
-        self.btnIndClear.grid(row=5, column=3, columnspan=2)
+        self.btnIndClear.grid(row=5, column=6, columnspan=2)
         
         ttk.Separator(self, orient=HORIZONTAL).grid(row=6, column=0,
-                                                    columnspan=5, sticky="ew")    
+                                                    columnspan=8, sticky="ew")    
         
         self.chkBtnP = BooleanVar()
         Checkbutton(self, text = "P",
@@ -146,21 +150,30 @@ class Application(Frame):
         Checkbutton(self, text = "T",
                     variable = self.chkBtnT
                     ).grid(row = 7, column = 4)
-     
-        
-        ttk.Separator(self, orient=HORIZONTAL).grid(row=12, column=0,
-                                                    columnspan=3, sticky="ew")
+    
+        self.chkBtnU = BooleanVar()
+        Checkbutton(self, text = "U",
+                    variable = self.chkBtnU
+                    ).grid(row = 7, column = 5)
+    
+        self.chkBtnV = BooleanVar()
+        Checkbutton(self, text = "V",
+                    variable = self.chkBtnV
+                    ).grid(row = 7, column = 6)
+
+        ttk.Separator(self, orient=HORIZONTAL).grid(row=8, column=0,
+                                                    columnspan=8, sticky="ew")
         
         Label(self, text="Treasure").grid(row=13, column=0, columnspan=5,
                                                  sticky="w")
         
         self.btnGenerateReport = Button(self, text="Generate",
                                         command=self.generate_report)
-        self.btnGenerateReport.grid(row=13, column=3, columnspan=2)
+        self.btnGenerateReport.grid(row=13, column=6, columnspan=2)
 
 
-        self.textBoxReport = Text(self, width = 50, height = 20, wrap = WORD)
-        self.textBoxReport.grid(row=14, column =0, columnspan=5)
+        self.textBoxReport = Text(self, width = 50, height = 15, wrap = WORD)
+        self.textBoxReport.grid(row=14, column =0, columnspan=8)
 
     def clear_lair_types(self):
         """clear the lair type checkboxes"""
@@ -168,16 +181,32 @@ class Application(Frame):
     
     def clear_ind_types(self):
         """clear the lair type checkboxes"""
-        self.chkBtnP = self.chkBtnQ = False
+        self.chkBtnP.set(0)
+        self.chkBtnQ.set(0)
         
     def generate_report(self):
         """ calculate treasure and display the results"""
-        message = "treasure results go here."
+        message = "LAIR\n------------------------------\n"
+
+        message += "INDIVIDUAL\n------------------------------\n"
+        if self.chkBtnQ.get():
+            self.get_numbers("Q")
+            message += "\n" + str(self.multiplier)
+
+        message += "UNGUARDED\n------------------------------\n"
+        
         self.textBoxReport.delete(0.0,END)
         self.textBoxReport.insert(0.0, message)
+
         
+    def get_numbers(self, treasureType):
+        self.multiplier = \
+          simpledialog.askinteger("Input", "Type "+treasureType+" multiplier?",
+                                  minvalue=1, maxvalue=99)
+                                                  
         
 #main section
+print(randint(1,6))
 root = Tk()
 root.title("BF Treasure Generator")
 #root.geometry('500x500')
---
+++ TreasureType.py
@@ -0,0 +1,14 @@
+# TreasureType.py
+# Thorin Schmidt
+# 9/16/2018
+
+from random import randint
+
+def type_a():
+    copper = 0
+    silver = 0
+    electrum = 0
+    gold = 0
+    platinum = 0
+    #copper
+    if ran
