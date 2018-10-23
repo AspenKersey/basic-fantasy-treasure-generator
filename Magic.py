f32393adb3707f283f0c380bd6d208159bef0ecf
Thorin Schmidt <tschmidt@stcharlessd.org>
2018-10-23 18:18:28 -0500

    Update Magic.py

--- Magic.py
+++ Magic.py
@@ -4,6 +4,14 @@
 # 10/23/2018
 
 from dice import *
+from Armor import *
+from MiscMagic import *
+from Potions import *
+from Rings import *
+from Scrolls import *
+from Wands import *
+from Weapons import *
+
 
 def get_magic(howmany):
     '''randomly determine a category of magic item'''
